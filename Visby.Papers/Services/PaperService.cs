917603fb99e87975c49586e50a68d1a87b7c0eb3
liupangx <liupangx@163.com>
2019-12-09 22:10:51 +0800

    未完的关键词查询

--- Visby.Papers/Services/PaperService.cs
+++ Visby.Papers/Services/PaperService.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using System;
 namespace Visby.Papers.Services
 {
@@ -16,6 +17,10 @@ namespace Visby.Papers.Services
         {
             var p = await db.Papers.AsQueryable().Where(p => p.PaperId == id).FirstOrDefaultAsync();
             return p;
+        }
+        public async Task<List<Paper>> getPapeKeyword(List<String> Keywords)
+        {
+            var ps await db.Papers.AsQueryable().
         }
     }
 }

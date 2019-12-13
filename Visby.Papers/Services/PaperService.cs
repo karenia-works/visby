e5c32ea1907cd1fb2178907fdbee47299b3efbdc
liupangx <liupangx@163.com>
2019-12-13 14:39:02 +0800

    各种paper查找

--- Visby.Papers/Services/PaperService.cs
+++ Visby.Papers/Services/PaperService.cs
@@ -18,9 +18,26 @@ namespace Visby.Papers.Services
             var p = await db.Papers.AsQueryable().Where(p => p.PaperId == id).FirstOrDefaultAsync();
             return p;
         }
-        public async Task<List<Paper>> getPapeKeyword(List<String> Keywords)
+        public async Task<List<Paper>> getPaperKeyword(List<String> Keywords)
         {
-            var ps await db.Papers.AsQueryable().
+            var ps = await db.Papers.FromSqlRaw("select * from Papers where keyword <@ '{0}'", Keywords).ToListAsync();
+            return ps;
         }
+        public async Task<List<Paper>> GetPaperAuthor(List<String> authors)
+        {
+            var ps = await db.Papers.FromSqlRaw("select * from Papers where Author <@ '{0}'", authors).ToListAsync();
+            return ps;
+        }
+        public async Task<List<Paper>> GetPaperLocalAuthor(List<int> Localauthers)
+        {
+            var ps = await db.Papers.FromSqlRaw("select * from Papers where Localauthor <@'{0}'", Localauthers).ToListAsync();
+            return ps;
+        }
+        public async Task<List<Paper>> GetPaperSummery(String tgt)//记得 加索引
+        {
+            var ps = await db.Papers.FromSqlRaw("select *,MAX(similarity({0},Title),similarity({0},summary)) As sml from paper where (summary % '{0}') or (summary%{0}) order by sml,quote DESC", tgt).ToListAsync();
+            return ps;
+        }
+        public async Task<List<Paper>> GetPapers
     }
 }

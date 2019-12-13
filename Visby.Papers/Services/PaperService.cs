5f337e9fdebd5908f72f96b4205ec2287056ba1e
liupangx <liupangx@163.com>
2019-12-13 14:42:22 +0800

    统一命名ok

--- Visby.Papers/Services/PaperService.cs
+++ Visby.Papers/Services/PaperService.cs
@@ -25,22 +25,22 @@ namespace Karenia.Visby.Papers.Services
         }
         public async Task<List<Paper>> getPaperKeyword(List<String> Keywords)
         {
-            var ps = await db.Papers.FromSqlRaw("select * from Papers where keyword <@ '{0}'", Keywords).ToListAsync();
+            var ps = await _context.Papers.FromSqlRaw("select * from Papers where keyword <@ '{0}'", Keywords).ToListAsync();
             return ps;
         }
         public async Task<List<Paper>> GetPaperAuthor(List<String> authors)
         {
-            var ps = await db.Papers.FromSqlRaw("select * from Papers where Author <@ '{0}'", authors).ToListAsync();
+            var ps = await _context.Papers.FromSqlRaw("select * from Papers where Author <@ '{0}'", authors).ToListAsync();
             return ps;
         }
         public async Task<List<Paper>> GetPaperLocalAuthor(List<int> Localauthers)
         {
-            var ps = await db.Papers.FromSqlRaw("select * from Papers where Localauthor <@'{0}'", Localauthers).ToListAsync();
+            var ps = await _context.Papers.FromSqlRaw("select * from Papers where Localauthor <@'{0}'", Localauthers).ToListAsync();
             return ps;
         }
         public async Task<List<Paper>> GetPaperSummery(String tgt)//记得 加索引
         {
-            var ps = await db.Papers.FromSqlRaw("select *,MAX(similarity({0},Title),similarity({0},summary)) As sml from paper where (summary % '{0}') or (summary%{0}) order by sml,quote DESC", tgt).ToListAsync();
+            var ps = await _context.Papers.FromSqlRaw("select *,MAX(similarity({0},Title),similarity({0},summary)) As sml from paper where (summary % '{0}') or (summary%{0}) order by sml,quote DESC", tgt).ToListAsync();
             return ps;
         }
         public async Task<List<Paper>> GetPapers
